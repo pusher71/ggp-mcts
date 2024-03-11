0678eea5652d9db91fb375e3408caa1f6868dba6
Bogdan <cool.b987@yandex.ru>
2024-03-11 19:00:12 +0300

    Удалён лишний класс MonteCarloTreeSearch

--- src/main/java/org/ggp/base/player/gamer/statemachine/mcts/MCTSGamer.java
+++ src/main/java/org/ggp/base/player/gamer/statemachine/mcts/MCTSGamer.java
@@ -1,17 +1,30 @@
 package org.ggp.base.player.gamer.statemachine.mcts;
 
-import org.ggp.base.player.gamer.Gamer;
-import org.ggp.base.player.gamer.exception.*;
-import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
+import org.ggp.base.player.gamer.statemachine.mcts.model.GameModel;
+import org.ggp.base.player.gamer.statemachine.mcts.model.State;
 import org.ggp.base.player.gamer.statemachine.sample.SampleGamer;
-import org.ggp.base.util.game.Game;
-import org.ggp.base.util.gdl.grammar.GdlTerm;
 import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MCTSGamer extends SampleGamer {
+
+    private GameModel model;
+    private Map<String, Integer> searchBudget;
+
+    public MCTSGamer(GameModel model) {
+        this.model = model;
+        searchBudget = new HashMap<>();
+    }
+
+    public Move execute(State startState, Role choosingRole) {
+        return null;
+    }
 
     @Override
     public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
